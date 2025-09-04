f2da1adf3a6184b8d53083a7a049aae14e7d6762
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-04 17:51:16 -0400

    cree el modelo de jugadores

---
+++ Models/Jugadores.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResgistroJugadores.Models;
+
+public class Jugadores
+
+{
+    [Key]
+    public int JugadorId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+
+    [Range(1, int.MaxValue, ErrorMessage = "las partidas jugadas no pueden ser menor a 1")]
+
+    public int Partida { get; set; }
+
+
+}
