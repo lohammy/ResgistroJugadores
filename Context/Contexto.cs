ea8763aedc61be5dc59c4cfce8c3344ffe542ffc
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-04 17:59:02 -0400

    cree el contexto

---
+++ Context/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResgistroJugadores.Models;
+
+namespace ResgistroJugadores.Context;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Jugadores> Jugadores { get; set; }
+}
