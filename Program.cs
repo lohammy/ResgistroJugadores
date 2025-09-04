dd19a3ec1f26d34d4aed5532bee845a4d6b509eb
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-04 18:11:15 -0400

    inyecte el contexto

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using ResgistroJugadores.Components;
+using ResgistroJugadores.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
+
+
+
+
 
 var app = builder.Build();
 
