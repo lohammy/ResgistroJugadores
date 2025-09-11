56498170bf03c4bd9ac430673065604b6c02124b
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-11 18:47:11 -0400

    base de datos y migracion

---
+++ Migrations/20250911220802_inicial.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResgistroJugadores.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jugadores",
+                columns: table => new
+                {
+                    JugadorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(type: "TEXT", nullable: false),
+                    Partida = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugadores", x => x.JugadorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partidas",
+                columns: table => new
+                {
+                    PartidaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Jugador1Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Jugador2Id = table.Column<int>(type: "INTEGER", nullable: true),
+                    EstadoPartida = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    GanadorId = table.Column<int>(type: "INTEGER", nullable: true),
+                    TurnoJugadorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EstadoTablero = table.Column<string>(type: "TEXT", maxLength: 9, nullable: false),
+                    FechaInicio = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partidas", x => x.PartidaId);
+                    table.ForeignKey(
+                        name: "FK_Partidas_Jugadores_GanadorId",
+                        column: x => x.GanadorId,
+                        principalTable: "Jugadores",
+                        principalColumn: "JugadorId");
+                    table.ForeignKey(
+                        name: "FK_Partidas_Jugadores_Jugador1Id",
+                        column: x => x.Jugador1Id,
+                        principalTable: "Jugadores",
+                        principalColumn: "JugadorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Partidas_Jugadores_Jugador2Id",
+                        column: x => x.Jugador2Id,
+                        principalTable: "Jugadores",
+                        principalColumn: "JugadorId");
+                    table.ForeignKey(
+                        name: "FK_Partidas_Jugadores_TurnoJugadorId",
+                        column: x => x.TurnoJugadorId,
+                        principalTable: "Jugadores",
+                        principalColumn: "JugadorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_GanadorId",
+                table: "Partidas",
+                column: "GanadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_Jugador1Id",
+                table: "Partidas",
+                column: "Jugador1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_Jugador2Id",
+                table: "Partidas",
+                column: "Jugador2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_TurnoJugadorId",
+                table: "Partidas",
+                column: "TurnoJugadorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Partidas");
+
+            migrationBuilder.DropTable(
+                name: "Jugadores");
+        }
+    }
+}
