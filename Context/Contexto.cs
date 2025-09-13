dc2bad9f54293d189fe9599c801c2f43c7616ad6
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-13 18:14:12 -0400

    onModelCreating

--- Context/Contexto.cs
+++ Context/Contexto.cs
@@ -6,7 +6,39 @@ namespace ResgistroJugadores.Context;
 public class Contexto : DbContext
 {
     public DbSet<Jugadores> Jugadores { get; set; }
-    public DbSet<Partidas> Partidas { get; set; }
+    public DbSet<Partida> Partidas { get; set; }
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        // clave foranea para Jugador1
+        modelBuilder.Entity<Partida>()
+            .HasOne(p => p.Jugador1)
+            .WithMany()
+            .HasForeignKey(p => p.Jugador1Id)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        // clave foranea para Jugador2
+        modelBuilder.Entity<Partida>()
+            .HasOne(p => p.Jugador2)
+            .WithMany()
+            .HasForeignKey(p => p.Jugador2Id)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        // clave foranea para Ganador
+        modelBuilder.Entity<Partida>()
+            .HasOne(p => p.Ganador)
+            .WithMany()
+            .HasForeignKey(p => p.GanadorId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        // clave foranea para turno de jugador
+        modelBuilder.Entity<Partida>()
+            .HasOne(p => p.TurnoJugador)
+            .WithMany()
+            .HasForeignKey(p => p.TurnoJugadorId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
 }
+
