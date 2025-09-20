961db1b7796ec6feb5ad5d47a661c62084537195
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-20 18:15:41 -0400

    se agrego al modelo el apartado de movimientos

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ResgistroJugadores.Models;
 
@@ -20,5 +21,8 @@ public class Jugadores
     public int Empates { get; set; }
 
     public int Derrotas { get; set; }
+
+    [InverseProperty(nameof(Models.Movimientos.Jugadores))]
+    public virtual ICollection<Movimientos> Movimientos { get; set; }
 
 }
---
+++ Models/Movimientos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResgistroJugadores.Models
+{
+    public class Movimientos
+    {
+        [Key]
+        public int MovimientoId { get; set; }
+        public int PartidaId { get; set; }
+        public int JugadorId { get; set; }
+        [Range(0, 2)]
+        public int PosicionFila { get; set; }
+        [Range(0, 2)]
+        public int PosicionColumna { get; set; }
+        public DateTime FechaMovimiento { get; set; } = DateTime.UtcNow;
+
+
+        ///Propiedades de navegacion
+        [ForeignKey(nameof(PartidaId))]
+        public virtual Partida Partidas { get; set; }
+
+        [ForeignKey(nameof(JugadorId))]
+        public virtual Jugadores Jugadores { get; set; }
+
+    }
+}
